6511640ae64f541824c34476c089ed3a84ce20bc
natanbc <natanbc@users.noreply.github.com>
2022-10-20 00:38:14 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+    python = pkgs.python310.withPackages (python-packages: with python-packages; [
+        imageio
+        numpy
+        scipy
+    ]);
+
+    unstable = import (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/master) {};
+in
+pkgs.mkShell {
+    nativeBuildInputs = [
+        unstable.ghdl
+        python
+        unstable.yosys
+    ];
+
+    shellHook = ''
+        export GHDL_PLUGIN=${unstable.yosys-ghdl}/share/yosys/plugins/ghdl.so
+    '';
+}
+
